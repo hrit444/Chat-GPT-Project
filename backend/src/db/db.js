04506ad553a93d340058e5867fa496187bcf0631
hritam04.samanta@gmail.com <hritam04.samanta@gmail.com>
2025-11-12 08:44:48 +0530

    first commit

---
+++ backend/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log("Connected to DB");
+    
+  } catch (error) {
+    console.error("Error connecting to mongoDB",err)
+  }
+}
+
+module.exports = connectDB
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/user.model")
+
+async function authUser(req,res,next) {
+  
+  const {token} = req.cookies
+
+  if(!token){
+    return res.status(401).json({message: "Unauthorized"})
+  }
+
+  try{
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await userModel.findById(decoded.id)
+
+    req.user = user
+
+    next()
+
+  }catch(err){
+    res.status(401).json({message: "Unauthorized"})
+  }
+}
+
+module.exports = {
+  authUser
+}
