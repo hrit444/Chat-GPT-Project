3b0f2545a6abad1c837debf3f7e9a66806f3d351
hritam04.samanta@gmail.com <hritam04.samanta@gmail.com>
2025-11-14 18:03:30 +0530

    AI integration

---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,19 @@
+const { GoogleGenAI } =  require("@google/genai");
+
+const ai = new GoogleGenAI({
+  apiKey:process.env.GEMINI_API_KEY
+});
+
+async function generateResponse(content){
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: content
+  })
+
+  console.log(response.text);
+  
+  return response.text
+}
+
+module.exports = {generateResponse}
